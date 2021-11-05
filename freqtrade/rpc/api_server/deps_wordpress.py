83c792d17b9ff1196c99798c3859086cdc6f3480
kecheon <kecheon@gmail.com>
2021-11-05 12:48:31 +0900

    switch auth endpoint to bot-config from wordpress

--- freqtrade/rpc/api_server/deps_wordpress.py
+++ freqtrade/rpc/api_server/deps_wordpress.py
@@ -13,47 +13,47 @@ def verify_auth_from_wordpress_api(api_config, username: str, password: str, req
 #  else:
     data = {
         "username": username,
-        "password": password
+        "password": password,
+        "referrer": request_url
     }
     return login_user(data)
 
 
 def login_user(data: Dict[str, str]) -> Boolean:
-        auth_key_for_bitsbee = os.getenv('AUTH_KEY_BITSBEE')
+    # auth_key is disabled in bitsbee.io
+    # auth_key_for_bitsbee = os.getenv('AUTH_KEY_BITSBEE')
+    try:
+        # fetch jwt from bitsbee.io API(wordpress jwt auth api)
+        endpoint = 'https://api.bitsbee.io/auth/login'
+        params = {
+            "username": data.get('username'),
+            "password": data.get('password'),
+            "referrer": data.get('referrer')
+        }
+
+        res = requests.post(endpoint, json=params)
         try:
-            # fetch jwt from bitsbee.io API(wordpress jwt auth api)
-            endpoint = 'https://bitsbee.io'
-            params = {
-                "rest_route": "/simple-jwt-login/v1/auth",
-                "email": data.get('username'),
-                "password": data.get('password'),
-                "AUTH_KEY": auth_key_for_bitsbee
-            }
-
-            res = requests.post(endpoint, data=params)
-            try:
-                data = res.json()
-                if data['success']:
-                    jwt = data['data']['jwt']
-                    response_object = {
-                        'status': 'success',
-                        'message': 'Successfully logged in.',
-                        'access_token': jwt
-                    }
-                    return True
-                else:
-                  return False
-
-            except:
+            data = res.json()
+            if data['status'] == 'success':
+                jwt = data['Authorization']
                 response_object = {
-                    'status': 'fail',
-                    'message': 'email or password does not match.'
+                    'status': 'success',
+                    'message': 'Successfully logged in.',
+                    'access_token': jwt
                 }
+                return True
+            else:
                 return False
-        except Exception as e:
+
+        except:
             response_object = {
                 'status': 'fail',
-                'message': 'Try again'
+                'message': 'email or password does not match.'
             }
             return False
-
+    except Exception as e:
+        response_object = {
+            'status': 'fail',
+            'message': 'Try again'
+        }
+        return False
